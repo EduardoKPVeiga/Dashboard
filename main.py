2c17cc76c8d35b101c28d0d96a9b319ab6c01217
EduardoKPVeiga <epontesveiga@gmail.com>
2024-06-09 21:30:15 -0300

    version_info working

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import ctypes
 
 _sys = ctypes.CDLL("./sys_call.so")
+_sys.version_info.restype = ctypes.c_char_p;
 
 def main():
     global _sys
-    _sys.hello()
+    var = _sys.version_info()
+    print("version info: ", var);
 
 if __name__ == "__main__":
     main()
