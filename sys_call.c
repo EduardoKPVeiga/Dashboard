d0caa3d8d736c8584a56d0eb6b00e96e53e54a9d
EduardoKPVeiga <epontesveiga@gmail.com>
2024-06-09 21:34:01 -0300

    read_sys_info update

--- sys_call.c
+++ sys_call.c
@@ -24,8 +24,8 @@ char* read_sys_info(const char* f_path, uint32_t string_size)
     // Buffer used store info
     char* info = (char*)calloc(string_size, sizeof(char));
     
-    // File descriptor
-    int file_d = open(f_path, O_RDONLY); 
+    // Opening file
+    int file_d = open(f_path, O_RDONLY); // File descriptor
     if (file_d < 0) { 
         perror("r1"); 
         exit(1);
@@ -35,15 +35,18 @@ char* read_sys_info(const char* f_path, uint32_t string_size)
     size_t data_size = string_size / 8;
     size_t size = read(file_d, info, data_size);
     info[size] = '\0'; // To be read as string
+
+    // Closing file
+    if (close(file_d) < 0) { 
+        perror("c1"); 
+        exit(1);
+        return "";
+    } 
+
     return info;
 }
 
 char* version_info()
 {
     return read_sys_info("/proc/version", 2048);
-}
-
-void hello(void)
-{
-    printf("%s", version_info());
 }
